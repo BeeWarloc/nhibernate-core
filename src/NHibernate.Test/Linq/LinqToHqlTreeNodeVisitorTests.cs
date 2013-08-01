c71f327de6af43beb715206de7a2dcc7fef0f833
Karsten Nikolai Strand <karsten.strand@okb.no>
2013-08-01 18:39:29 +0200

    Added test checking that hql visitor gets called.

---
+++ src/NHibernate.Test/Linq/LinqToHqlTreeNodeVisitorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using NHibernate.Cfg;
+using NHibernate.Hql.Ast;
+using NHibernate.Linq.Functions;
+using NUnit.Framework;
+using Environment = NHibernate.Cfg.Environment;
+
+namespace NHibernate.Test.Linq
+{
+	[TestFixture]
+	public class LinqToHqlTreeNodeVisitorTests : LinqTestCase
+	{
+		[ThreadStatic] private static bool testNodeVisitorHasBeenCalled;
+
+		private class TestNodeVisitor : ILinqToHqlTreeNodeVisitor
+		{
+			public void Visit(HqlTreeNode hqlTree)
+			{
+				testNodeVisitorHasBeenCalled = true;
+			}
+		}
+
+		protected override void Configure(Configuration configuration)
+		{
+			configuration.Properties[Environment.LinqToHqlTreeNodeVisitor] =
+				typeof (TestNodeVisitor).AssemblyQualifiedName;
+			base.Configure(configuration);
+		}
+
+		[Test]
+		public void LinqToHqlTreeNodeVisitorIsCalledDuringQuery()
+		{
+			try
+			{
+				db.Orders.Where(x => x.OrderId >= 1).ToList();
+				Assert.IsTrue(testNodeVisitorHasBeenCalled, "Test node visitor was not called.");
+			}
+			finally
+			{
+				testNodeVisitorHasBeenCalled = false;
+			}
+		}
+	}
+}
